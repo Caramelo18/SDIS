7fddf585c5fd69277d86753b3d6586495a7ba5a2
Eduardo <xJupiter931>
2017-03-07 12:33:25 +0000

    Restructuring

--- Project1/src/peer/Peer.java
+++ Project1/src/peer/Peer.java
@@ -3,6 +3,7 @@ package peer;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import files.FileManager;
 import socket_listener.SocketListener;
 
 public class Peer
@@ -11,6 +12,9 @@ public class Peer
 	private static SocketListener MC;
 	private static SocketListener MDB;
 	private static SocketListener MDR;
+	
+	private static String file = "";
+	private static Integer replicationDeg = 0;
 
 	public static void main(String[] args)
 	{
@@ -18,23 +22,30 @@ public class Peer
 		
 		try
 		{
-			InetAddress MC_Address = InetAddress.getByName(args[1]);
-			Integer MC_Port = Integer.parseInt(args[2]);
-			MC = new SocketListener(MC_Address, MC_Port);
+			// InetAddress MC_Address = InetAddress.getByName(args[1]);
+			// Integer MC_Port = Integer.parseInt(args[2]);
+			MC = new SocketListener(InetAddress.getByName("224.1.1.1"), 5000);
 			
-			InetAddress MDB_Address = InetAddress.getByName(args[3]);
-			Integer MDB_Port = Integer.parseInt(args[4]);
-			MDB = new SocketListener(MDB_Address, MDB_Port);
+			// InetAddress MDB_Address = InetAddress.getByName(args[3]);
+			// Integer MDB_Port = Integer.parseInt(args[4]);
+			MDB = new SocketListener(InetAddress.getByName("224.2.2.2"), 5001);
 			
-			InetAddress MDR_Address = InetAddress.getByName(args[5]);
-			Integer MDR_Port = Integer.parseInt(args[6]);
-			MDR = new SocketListener(MDR_Address, MDR_Port);
+			// InetAddress MDR_Address = InetAddress.getByName(args[5]);
+			// Integer MDR_Port = Integer.parseInt(args[6]);
+			MDR = new SocketListener(InetAddress.getByName("224.3.3.3"), 5002);
 		}
 		catch (UnknownHostException e)
 		{
 			e.printStackTrace();
 		}
 		
+		FileManager FM = new FileManager();
+		
+		if(args.length == 2)
+		{
+			file = args[1];
+			replicationDeg = Integer.parseInt(args[2]);
+		}
 	}
 
 }
