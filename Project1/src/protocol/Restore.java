5844990b7306f706bea769a68ed29ae74fc7b2fc
Eduardo <xJupiter931>
2017-03-29 20:22:48 +0100

    Added failsafes for backup

--- Project1/src/protocol/Restore.java
+++ Project1/src/protocol/Restore.java
@@ -1,5 +1,6 @@
 package protocol;
 
+import java.io.File;
 import java.util.HashMap;
 
 import data.DataManager;
@@ -29,17 +30,33 @@ public class Restore implements Runnable
 			System.out.println("No such file backed up");
 			return;
 		}
+		
+		File f = new File("../Peer" + Peer.getServerId() + "/Files/" + filename);
+		if(f.exists())
+		{
+			System.out.println("Can't restore a file that is already in the files folder");
+			return;
+		}
 
 		boolean running = true;
 		int chunkNo = 0;
+		int attempts = 0;
+		
 		while(running)
 		{
+			if(attempts > 3)
+			{
+				System.out.println("Can't restore chunk");
+				return;
+			}
+			
 			byte[] message = MessageGenerator.generateGETCHUNK(fileId, chunkNo);
 			Peer.getMDB().sendPacket(message);
 			
 			long startTime = System.nanoTime();
 			boolean pooling = true;
 			
+			boolean found = false;
 			while(pooling)
 			{
 				byte[] received = ChunkRec.getMessage(fileId, chunkNo);
@@ -47,10 +64,12 @@ public class Restore implements Runnable
 				{
 					fileParts.put(chunkNo, received);
 					chunkNo++;
+					attempts = 0;
 					
 					if(received.length < 64000)
 						running = false;
 					
+					found = true;
 					pooling = false;
 				}
 				
@@ -59,6 +78,9 @@ public class Restore implements Runnable
 					pooling = false;
 				}
 			}
+			
+			if(!found)
+				attempts++;
 		}
 		
 		FileManager.restoreFile(fileParts);
